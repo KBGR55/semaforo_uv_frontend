ba5b7b884db113ef0d370ccfd49a49a8bc7fabfb
KBGR55 <brigithgonzaga501@gmail.com>
2024-01-09 10:26:03 -0500

    versión_2.6

--- src/fragment/Registro.jsx
+++ src/fragment/Registro.jsx
@@ -1,19 +1,58 @@
-import React from 'react';
-import '../css/style.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useForm } from 'react-hook-form';
+import { LoginPost, PeticionGet } from '../hooks/Conexion';
+import { getRol, getToken, saveRol, saveToken, saveUser } from '../utilidades/Sessionutil';
+import { useNavigate } from 'react-router';
+import mensajes from '../utilidades/Mensajes';
+import logo from '../logo.png';
 import Footer from './Footer';
 import BarraMenu from './BarraMenu';
-import logo from '../logo.png';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import '../css/style.css';
 
-const Registro = () => (
-  <div className="background-radial-gradient overflow-hidden">
-    <header>
-      <BarraMenu />
-    </header>
-    <section>
-      <div className="container px-4 py-5 px-md-5 text-center text-lg-start my-5">
-        <div className="row gx-lg-5 align-items-center mb-5">
-          <div className="col-lg-7 mb-lg-0 position-relative">
+const Registro = () => {
+  const navegation = useNavigate();
+  const { register, handleSubmit, formState: { errors }, watch } = useForm();
+
+  const onSubmit = (data) => {
+    var datos = {
+      "nombres": data.nombres,
+      "clave": data.clave
+    };
+    /*
+    LoginPost(datos,'cuenta/sesion').then((info) => {  
+        if (info.code !== 200) { 
+            mensajes(info.msg, "error", "error");
+        } else {
+            saveToken(info.token);
+            console.log("Token:" + getToken());
+            console.log("Usuario: " + info.user);
+            saveUser(info.user);
+            saveRol(info.rol);
+            navegation("/api");
+            mensajes(info.msg);
+        };
+        });*/
+  };
+
+  const nombresValue = watch('nombres');
+  const apellidosValue = watch('apellidos');
+  const fecha_nacimientoValue = watch('fecha_nacimiento');
+  const telefonoValue = watch('telefono');
+  const direccionValue = watch('direccion');
+  const cargoValue = watch('cargo');
+  const institucionValue = watch('institucion');
+  const correoValue = watch('correo');
+  const claveValue = watch('clave');
+
+  return (
+    <div className="background-radial-gradient overflow-hidden">
+      <header>
+        <BarraMenu />
+      </header>
+      <section>
+        <div className="container align-items-center text-center text-lg-start my-5">
+
+          <div className="mb-lg-0">
             <div id="radius-shape-1" className="position-absolute rounded-circle shadow-5-strong"></div>
             <div id="radius-shape-2" className="position-absolute shadow-5-strong"></div>
 
@@ -30,60 +69,77 @@ const Registro = () => (
                   <div className="col-md-6 col-lg-7 d-flex align-items-center">
                     <div className="card-body">
                       <div className="container">
-                        <form className="row g-3">
+                        <form className="row g-3" onSubmit={handleSubmit(onSubmit)}>
                           <div className="col-md-4">
-                            <div className="form-outline">
+                            <div className={`input-field form-outline ${nombresValue ? 'active' : ''}`} >
                               <em className="far fa-user"></em>
-                              <input type="text" className="form-control" placeholder="Nombres" />
+                              <input type="text" className="form-control" placeholder="Nombres" {...register('nombres', { required: true })} />
                             </div>
+                            {errors.nombres && errors.nombres.type === 'required' && <div className='alert alert-danger'>Ingrese sus nombre</div>}
+                            {errors.nombres && errors.nombres.type === 'pattern' && <div className='alert alert-danger'>Ingrese sus nombres</div>}
                           </div>
                           <div className="col-md-4">
-                            <div className="form-outline">
+                            <div className={`input-field form-outline ${apellidosValue ? 'active' : ''}`} >
                               <em className="far fa-user"></em>
-                              <input type="email" className="form-control" placeholder="Apellidos" />
+                              <input type="email" className="form-control" placeholder="Apellidos" {...register('apellidos', { required: true })} />
                             </div>
+                            {errors.apellidos && errors.apellidos.type === 'required' && <div className='alert alert-danger'>Ingrese sus apellidos</div>}
+                            {errors.apellidos && errors.apellidos.type === 'pattern' && <div className='alert alert-danger'>Ingrese sus apellidos</div>}
                           </div>
                           <div className="col-md-4">
-                            <div className="form-outline">
+                            <div className={`input-field form-outline ${direccionValue ? 'active' : ''}`} >
                               <em className="far fa-map"></em>
-                              <input type="text" className="form-control" placeholder="Dirección" />
+                              <input type="text" className="form-control" placeholder="Dirección" {...register('direccion', { required: true })} />
                             </div>
+                            {errors.direccion && errors.direccion.type === 'required' && <div className='alert alert-danger'>Ingrese su dirección</div>}
+                            {errors.direccion && errors.direccion.type === 'pattern' && <div className='alert alert-danger'>Ingrese su dirección</div>}
                           </div>
                           <div className="col-md-4">
-                            <div className="form-outline">
+                            <div className={`input-field form-outline ${fecha_nacimientoValue ? 'active' : ''}`} >
                               <em className="far fa-calendar"></em>
-                              <input type="text" className="form-control" placeholder="Fecha Nacimiento" />
+                              <input type="text" className="form-control" placeholder="Fecha Nacimiento" {...register('fecha_nacimiento', { required: true })} />
                             </div>
+                            {errors.fecha_nacimiento && errors.fecha_nacimiento.type === 'required' && <div className='alert alert-danger'>Ingrese su fecha de nacimiento</div>}
+                            {errors.fecha_nacimiento && errors.fecha_nacimiento.type === 'pattern' && <div className='alert alert-danger'>Ingrese su fecha de nacimiento</div>}
                           </div>
                           <div className="col-md-4">
-                            <div className="form-outline">
+                            <div className={`input-field form-outline ${telefonoValue ? 'active' : ''}`} >
                               <em className="fa fa-phone"></em>
-                              <input type="text" className="form-control" placeholder="Teléfono" />
+                              <input type="text" className="form-control" placeholder="Teléfono" {...register('telefono', { required: true })} />
                             </div>
+                            {errors.telefono && errors.telefono.type === 'required' && <div className='alert alert-danger'>Ingrese su telefono</div>}
+                            {errors.telefono && errors.telefono.type === 'pattern' && <div className='alert alert-danger'>Ingrese su telefono</div>}
                           </div>
                           <div className="col-md-4">
-                            <div className="form-outline">
-                              <em className="fa fa-briefcase"></em>
-                              <input type="text" className="form-control" placeholder="Cargo" />
-                            </div>
-                          </div>
-                          <div className="col-md-4">
-                            <div className="form-outline">
+                            <div className={`input-field form-outline ${institucionValue ? 'active' : ''}`} >
                               <em className="far fa-building"></em>
-                              <input type="text" className="form-control" placeholder="Institución" />
+                              <input type="text" className="form-control" placeholder="Institución" {...register('institucion', { required: true })} />
                             </div>
+                            {errors.institucion && errors.institucion.type === 'required' && <div className='alert alert-danger'>Ingrese la institución</div>}
+                            {errors.institucion && errors.institucion.type === 'pattern' && <div className='alert alert-danger'>Ingrese el cargo en la institución</div>}
                           </div>
                           <div className="col-md-4">
-                            <div className="form-outline">
-                              <em className="far fa-envelope"></em>
-                              <input type="text" className="form-control" placeholder="Correo" />
+                            <div className={`input-field form-outline ${cargoValue ? 'active' : ''}`} >
+                              <em className="fa fa-briefcase"></em>
+                              <input type="text" className="form-control" placeholder="Cargo" {...register('cargo', { required: true })} />
                             </div>
+                            {errors.cargo && errors.cargo.type === 'required' && <div className='alert alert-danger'>Ingrese su cargo en la institución</div>}
+                            {errors.cargo && errors.cargo.type === 'pattern' && <div className='alert alert-danger'>Ingrese su cargo en la institución</div>}
                           </div>
                           <div className="col-md-4">
-                            <div className="form-outline">
-                              <em className="fas fa-key"></em>
-                              <input type="password" className="form-control" placeholder="Clave" />
+                            <div className={`input-field form-outline ${correoValue ? 'active' : ''}`} >
+                              <em className="far fa-user"></em>
+                              <input type="email"  className="form-control"  placeholder="Correo" {...register('correo', { required: true, pattern: /^\S+@\S+$/i })} />
                             </div>
+                            {errors.correo && errors.correo.type === 'required' && <div className='alert alert-danger'>Ingrese el correo</div>}
+                            {errors.correo && errors.correo.type === 'pattern' && <div className='alert alert-danger'>Ingrese un correo valido</div>}
+                          </div>
+                          <div className="col-md-4">
+                            <div className={`input-field form-outline ${claveValue ? 'active' : ''}`}>
+                              <em class="fas fa-lock "></em>
+                              <input type="password"  className="form-control" placeholder="Clave" {...register('clave', { required: true })} />
+                            </div>
+                            {errors.clave && errors.clave.type === 'required' && <div className='alert alert-danger'>Ingrese una clave</div>}
                           </div>
                           <div className="col-12 d-flex justify-content-end">
                             <button type="submit" className="btn btn-primary">Registrarme</button>
@@ -96,17 +152,12 @@ const Registro = () => (
               </div>
             </div>
           </div>
-          <div className="col-lg-5 mb-5 mb-lg-0" style={{ zIndex: 10 }}>
-            <h1 className="my-5 display-5 fw-bold ls-tight" style={{ color: 'hsl(218, 81%, 95%)' }}>
-              Registrar Usuarios<br />
 
-            </h1>
-          </div>
         </div>
-      </div>
-    </section>
-    <Footer />
-  </div>
-);
+      </section>
+      <Footer />
+    </div>
+  );
+}
 
 export default Registro;
